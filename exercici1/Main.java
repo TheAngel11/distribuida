fdbf18c0cc0840c94f778fc88de4407681dd1307
Biel Carpi <biel.carpi@outlook.com>
2023-10-09 19:19:34 +0200

    feat(exercici1): Minor fixes & add Timer

--- exercici1/Main.java
+++ exercici1/Main.java
@@ -9,16 +9,19 @@ public class Main {
         //llSearch.startSearch();
 
         //Exercise 4
-        final int ARRAY_POSITIONS = 10000;
-        final int INT_TO_SEARCH = 3000;
+        final int ARRAY_POSITIONS = 12000000;
+        final int INT_TO_SEARCH = 2500;
         final int NUM_THREADS = 6;
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < ARRAY_POSITIONS; i++)
+        for (int i = 0; i < ARRAY_POSITIONS; i++)
             list.add(i);
 
-        //Collections.shuffle(list);
+        Collections.shuffle(list);
+        System.out.println("Searching for " + INT_TO_SEARCH + " with " + NUM_THREADS + " threads on an array with " + ARRAY_POSITIONS + " positions");
         int[] array = list.stream().mapToInt(Integer::intValue).toArray();
-        int threadID = ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
-        System.out.println("Thread " + threadID + " found the target");
+        long startTime = System.currentTimeMillis();
+        int box = ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Found the target at index " + box + " with " + (endTime-startTime) + "ms.");
     }
 }
--- exercici1/ParallelArraySearch.java
+++ exercici1/ParallelArraySearch.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 
 public class ParallelArraySearch {
 
+    private static volatile boolean found = false;
+
     public static int ParallelSearch(int target, int[] array, int numThreads){
+        if(array.length%numThreads != 0){
+            throw new RuntimeException("Length of the array is not divisible by numThreads");
+        }
+
         int slice = array.length/numThreads;
         Thread[] threads = new Thread[numThreads];
         int[] results = new int[numThreads];
@@ -13,33 +19,33 @@ public class ParallelArraySearch {
             threads[i] = new Thread(() -> {
                 results[threadNum] = search(target, split, threadNum + 1);
             });
+
             threads[i].start();
         }
-        int threadID = -1;
 
         for(int i = 0; i < numThreads; i++){
             try {
                 threads[i].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if(results[i] != -1){
-                threadID = results[i];
-                break;
-            }
+            } catch (InterruptedException ignored) {}
+
+            if(results[i] != -1) return results[i] + (i * slice);
         }
 
-        return threadID;
+        return -1;
     }
 
     public static int search(int target, int[] array, int threadNum){
         for(int i = 0; i < array.length; i++){
+            if(found) break;
+
             if(array[i] == target){
-                System.out.println("Thread found target at index: " + i);
-                return threadNum;
+                System.out.println("Thread " + threadNum + " found target at index: " + i);
+                found = true;
+                return i;
             }
         }
 
+        System.out.println("Stopping thread " + threadNum);
         return -1;
     }
 }
