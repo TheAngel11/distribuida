e1e7aa84f07ce13244bb1130e4f17ceda9257416
Biel Carpi <biel.carpi@outlook.com>
2023-10-09 20:01:22 +0200

    feat(exercici1): Add ParallelSearchSharedMemory

--- exercici1/Main.java
+++ exercici1/Main.java
@@ -1,27 +1,62 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static final String MENU_MSG = """
+                                  Select an option to run (1, 2 or 3): "
+                            
+                                      1. Exercise 3
+                            
+                                      2. Exercise 4 & 5
+                             
+                                      3. Exercise 7 & 8
+                            
+                                  Insert your option:\s""";
+
     public static void main(String[] args) {
-        //Exercise 3
-        //LinkedListSearch llSearch = new LinkedListSearch(10000);
-        //llSearch.startSearch();
+        System.out.print(MENU_MSG);
+        //int option = Integer.parseInt(new Scanner(System.in).nextLine());
+        int option = 2;
 
-        //Exercise 4
-        final int ARRAY_POSITIONS = 12000000;
-        final int INT_TO_SEARCH = 2500;
-        final int NUM_THREADS = 6;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < ARRAY_POSITIONS; i++)
-            list.add(i);
+        switch(option){
+            case 1:
+                //Exercise 3
+                LinkedListSearch llSearch = new LinkedListSearch(10000);
+                llSearch.startSearch();
+                break;
+            case 2:
+                //Exercise 4 & 5
+                final int ARRAY_POSITIONS = 140000000;
+                final int INT_TO_SEARCH = 250000;
+                final int NUM_THREADS = 4;
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < ARRAY_POSITIONS; i++)
+                    list.add(i);
 
-        Collections.shuffle(list);
-        System.out.println("Searching for " + INT_TO_SEARCH + " with " + NUM_THREADS + " threads on an array with " + ARRAY_POSITIONS + " positions");
-        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
-        long startTime = System.currentTimeMillis();
-        int box = ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Found the target at index " + box + " with " + (endTime-startTime) + "ms.");
+                //Collections.shuffle(list);
+                System.out.println("Searching for " + INT_TO_SEARCH + " with " + NUM_THREADS + " threads on an array with " + ARRAY_POSITIONS + " positions\n");
+                int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+
+                System.out.println("With Shared Memory --> ");
+                long startTime = System.currentTimeMillis();
+                int box = ParallelArraySearch.ParallelSearchSharedMemory(INT_TO_SEARCH, array, NUM_THREADS);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Found the target at index " + box + " with " + (endTime-startTime) + "ms.");
+
+                System.out.println("With Shared Nothing --> ");
+                startTime = System.currentTimeMillis();
+                box = ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
+                endTime = System.currentTimeMillis();
+                System.out.println("Found the target at index " + box + " with " + (endTime-startTime) + "ms.");
+                break;
+
+            case 3:
+                // Exercise 7 & 8
+                break;
+            default:
+                System.out.println("Select a valid option (1, 2 or 3)");
+        }
     }
 }
--- exercici1/ParallelArraySearch.java
+++ exercici1/ParallelArraySearch.java
@@ -2,20 +2,21 @@ import java.util.Arrays;
 
 public class ParallelArraySearch {
 
-    private static volatile boolean found = false;
+    private static volatile boolean found;
 
-    public static int ParallelSearch(int target, int[] array, int numThreads){
-        if(array.length%numThreads != 0){
+    public static int ParallelSearch(int target, int[] array, int numThreads) {
+        if (array.length % numThreads != 0) {
             throw new RuntimeException("Length of the array is not divisible by numThreads");
         }
 
-        int slice = array.length/numThreads;
+        found = false;
+        int slice = array.length / numThreads;
         Thread[] threads = new Thread[numThreads];
         int[] results = new int[numThreads];
 
-        for(int i = 0; i < numThreads; i++){
+        for (int i = 0; i < numThreads; i++) {
             final int threadNum = i;
-            int[] split = Arrays.copyOfRange(array, slice*i, slice*(i+1));
+            int[] split = Arrays.copyOfRange(array, slice * i, slice * (i + 1));
             threads[i] = new Thread(() -> {
                 results[threadNum] = search(target, split, threadNum + 1);
             });
@@ -23,29 +24,76 @@ public class ParallelArraySearch {
             threads[i].start();
         }
 
-        for(int i = 0; i < numThreads; i++){
+        for (int i = 0; i < numThreads; i++) {
             try {
                 threads[i].join();
-            } catch (InterruptedException ignored) {}
+            } catch (InterruptedException ignored) {
+            }
 
-            if(results[i] != -1) return results[i] + (i * slice);
+            if (results[i] != -1) return results[i] + (i * slice);
         }
 
         return -1;
     }
 
-    public static int search(int target, int[] array, int threadNum){
-        for(int i = 0; i < array.length; i++){
-            if(found) break;
+    public static int search(int target, int[] array, int threadNum) {
+        for (int i = 0; i < array.length; i++) {
+            if (found) break;
 
-            if(array[i] == target){
+            if (array[i] == target) {
                 System.out.println("Thread " + threadNum + " found target at index: " + i);
                 found = true;
                 return i;
             }
         }
 
-        System.out.println("Stopping thread " + threadNum);
         return -1;
     }
+
+
+    public static int ParallelSearchSharedMemory(int target, int[] array, int numThreads) {
+        if (array.length % numThreads != 0) {
+            throw new RuntimeException("Length of the array is not divisible by numThreads");
+        }
+
+        found = false;
+        int slice = array.length / numThreads;
+        Thread[] threads = new Thread[numThreads];
+        int[] results = new int[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            final int threadNum = i;
+            threads[i] = new Thread(() -> {
+                results[threadNum] = searchSharedMemory(target, array, threadNum + 1, slice * threadNum, slice * (threadNum + 1));
+            });
+
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException ignored) {
+            }
+
+            if (results[i] != -1) return results[i] + (i * slice);
+        }
+
+        return -1;
+    }
+
+    public static int searchSharedMemory(int target, int[] array, int threadNum, int firstIndex, int lastIndex) {
+        for (int i = firstIndex; i < lastIndex; i++) {
+            if (found) break;
+
+            if (array[i] == target) {
+                System.out.println("Thread " + threadNum + " found target at index: " + i);
+                found = true;
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
