48b762576242bd9d702985096cc600c13efacd9d
angel.garcia <angel.garcia@students.salle.url.edu>
2023-10-09 18:29:23 +0200

    Did the exercice 4

--- exercici1/Main.java
+++ exercici1/Main.java
@@ -5,19 +5,20 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         //Exercise 3
-        LinkedListSearch llSearch = new LinkedListSearch(10000);
-        llSearch.startSearch();
+        //LinkedListSearch llSearch = new LinkedListSearch(10000);
+        //llSearch.startSearch();
 
         //Exercise 4
         final int ARRAY_POSITIONS = 10000;
-        final int INT_TO_SEARCH = 300;
-        final int NUM_THREADS = 5;
+        final int INT_TO_SEARCH = 3000;
+        final int NUM_THREADS = 6;
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i < ARRAY_POSITIONS; i++)
             list.add(i);
 
-        Collections.shuffle(list);
+        //Collections.shuffle(list);
         int[] array = list.stream().mapToInt(Integer::intValue).toArray();
-        ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
+        int threadID = ParallelArraySearch.ParallelSearch(INT_TO_SEARCH, array, NUM_THREADS);
+        System.out.println("Thread " + threadID + " found the target");
     }
 }
--- exercici1/ParallelArraySearch.java
+++ exercici1/ParallelArraySearch.java
@@ -4,21 +4,42 @@ public class ParallelArraySearch {
 
     public static int ParallelSearch(int target, int[] array, int numThreads){
         int slice = array.length/numThreads;
+        Thread[] threads = new Thread[numThreads];
+        int[] results = new int[numThreads];
 
         for(int i = 0; i < numThreads; i++){
+            final int threadNum = i;
             int[] split = Arrays.copyOfRange(array, slice*i, slice*(i+1));
-            Thread t = new Thread(() -> search(target, split));
-            t.start();
+            threads[i] = new Thread(() -> {
+                results[threadNum] = search(target, split, threadNum + 1);
+            });
+            threads[i].start();
+        }
+        int threadID = -1;
+
+        for(int i = 0; i < numThreads; i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(results[i] != -1){
+                threadID = results[i];
+                break;
+            }
+        }
+
+        return threadID;
+    }
+
+    public static int search(int target, int[] array, int threadNum){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == target){
+                System.out.println("Thread found target at index: " + i);
+                return threadNum;
+            }
         }
 
         return -1;
-    }
-
-    public static int search(int target, int[] array){
-        for(int i = 0; i < array.length; i++){
-            if(array[i] == target){
-                return;
-            }
-        }
     }
 }
